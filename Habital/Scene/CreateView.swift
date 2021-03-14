f98bcd70e8d99c2afe455d638bd1ad9f48c71f5d
Taishi Kusunose <taishi.kusunose@gmail.com>
2021-03-15 08:11:36 +0900

    create launchscreen

---
+++ Habital/Scene/CreateView.swift
@@ -0,0 +1,20 @@
+//
+//  CreateView.swift
+//  Habital
+//
+//  Created by Taishi Kusunose on 2021/03/14.
+//
+
+import SwiftUI
+
+struct CreateView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CreateView_Previews: PreviewProvider {
+    static var previews: some View {
+        CreateView()
+    }
+}
